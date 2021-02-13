7f3a1d5c857a1b2093f185de7d41f0e5bd84ee9b
Eric Li <hefe.mk@gmail.com>
2021-02-13 22:32:45 +0800

    Add HandCards & Poker

---
+++ src/components/HandCards.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="row">
+        <div class="col-md-2 col-sm-4 my-2" v-for="p in pokerPoints" :key="p">
+            <Poker 
+                :ref="setItemRef"
+                :point="p"
+                @on-card-play="onCardPlay" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Options, Vue } from 'vue-class-component';
+    import Poker from './Poker.vue';
+
+    @Options({
+        components: {
+            Poker,
+        },
+    })
+    export default class HandCards extends Vue {
+        private pokerEls: Poker[] = [];
+        pokerPoints: string[] = [
+            '0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144', '∞', '?', '☕'
+        ];
+
+        onCardPlay(cardPoint: string) {
+            this.pokerEls.forEach((poker: Poker) => {
+                poker.played = false;
+                if (poker.point === cardPoint) {
+                    poker.played = true;
+                }
+            });
+            console.log(this.pokerEls);
+        }
+
+        setItemRef(el: any) {
+            if (el) {
+                this.pokerEls.push(el);
+            }
+        }
+
+        onBeforeUpdate(): void {
+            this.pokerEls = [];
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Poker.vue
@@ -0,0 +1,38 @@
+<template>
+    <span class="card text-center"
+        :class="{ active: played }"
+        @click="play()">
+        {{ point }}
+    </span>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+    @Options({
+        props: {
+            point: {
+                type: String,
+                required: true
+            },
+        },
+        emits: ['on-card-play']
+    })
+    export default class Poker extends Vue {
+        point!: string;
+        played: boolean = false;
+
+        play(): void {
+            this.played = true;
+            this.$emit('on-card-play', this.point);
+        }
+    }
+</script>
+
+<style scoped>
+    .card {
+        cursor: pointer;
+    }
+    .active {
+        font-weight: bold;
+    }
+</style>
